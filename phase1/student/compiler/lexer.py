551b65dcbc8319e0640f68cd875a59ca712fcab8
Miald Saadat <msaadat1379@gmail.com>
2022-03-08 15:16:45 +0330

    start phase1

--- phase1/student/compiler/lexer.py
+++ phase1/student/compiler/lexer.py
@@ -1,6 +1,23 @@
-
+from lark import Lark
+from lark import Transformer
 # you can write here and import this file in main!
 
-def new_lexer():
-    return None
+rules = """
+    start : (ID)*
+    ID: /[a-zA-Z][a-zA-Z0-9_]*/
+    %import common.WS -> WS
+    %ignore WS
+"""
+all_tokens = []
+
+class T(Transformer):
+    def ID(self, token):
+        all_tokens.append(("T_ID " + token))
+        return "T_ID " + token
+
+def new_lexer(string):
+    all_tokens.clear()
+    parser = Lark(rules, parser='lalr', transformer = T())
+    parser.parse(string)
+    return '\n'.join(all_tokens)
 
