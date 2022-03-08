506a0f56db4548a5a7ee43c81ea46be238992929
ckodser <arshia.soltani2@gmail.com>
2022-03-08 21:22:39 +0330

    add runner for debugging

---
+++ phase1/easyRun.py
@@ -0,0 +1,28 @@
+from phase1.student.compiler.main import run
+from os import listdir
+
+
+def join_path(path):
+    return 'tests/test/in-out/' + path
+
+
+step = 0
+
+######################################
+# by setting only_one_test to a value different from None it will test only that testcase
+only_one_test = None
+######################################
+
+for f in listdir('tests/test/in-out'):
+    if f[-3:] == ".in":
+        step += 1
+        if only_one_test is not None and step != only_one_test:
+            continue
+
+        input = join_path(f)
+        output = join_path(f[:-3] + ".out")
+        t = run(input)
+        print("#" * 50, "test = ", step, "#" * 50)
+        print(open(input).read())
+        print("<" * 10 + "=" * 40, "test = ", step, "=" * 40 + ">" * 10)
+        print(t)
