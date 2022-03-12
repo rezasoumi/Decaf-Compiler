fa7d2a495b5ab66e47ea44bb2849ef087ad2a837
Reza Soumi <reza.soumi800@gmail.com>
2022-03-12 18:37:58 +0330

    add some comment.

--- phase1/student/compiler/__init__.py
+++ phase1/student/compiler/__init__.py
@@ -1,1 +1,3 @@
-
+from lark import Lark
+from lark import Transformer
+import re
--- phase1/student/compiler/lexer.py
+++ phase1/student/compiler/lexer.py
@@ -2,10 +2,8 @@ from lark import Lark
 from lark import Transformer
 import re
 
-
-# you can write here and import this file in main!
-
 def replace_line(define_map, line):
+    """replace line with defined map of previous line"""
     line = re.split('( |"|\[|]|\(|\))', line)
     for i in range(len(line)):
         if line[i] in define_map:
@@ -15,24 +13,25 @@ def replace_line(define_map, line):
         answer += i
     return answer
 
-
-def replace_defines(input):
-    input_array = input.split("\n")
-    define_map = {}
+def replace_defines(input_lines):
+    """replace lines with defined map with iteration"""
+    input_lines = input.split("\n")
+    define_map = {} 
     answer = ""
-    for line in input_array:
+    for line in input_lines:
         line = replace_line(define_map, line)
         split_form = line.split()
+        # true define format => define x y => y can have \t
         if len(split_form) >= 3 and split_form[0] == 'define':
+            # add key value of define to map
             define_map[split_form[1]] = " ".join(split_form[2:])
         else:
             answer += line + "\n"
     return answer
 
-
 rules = """
     start : (ID | OPERATOR_PUNC | INT_LIT | DOUBLE_LIT | COMMENT | STRING_LIT )*
-                
+    
     ID: /[a-zA-Z][a-zA-Z0-9_]*/ | /__func__[a-zA-Z0-9_]*/ | /__line__[a-zA-Z0-9_]*/ 
     OPERATOR_PUNC: "+" | "-" | "*" | "/" | "%" | "<" | "<=" | ">" | ">=" | "=" 
                     | "+=" | "-=" | "-+" | "*=" | "/=" | "==" | "!=" | "&&" |  "||" 
@@ -48,6 +47,8 @@ rules = """
 """
 all_tokens = []
 
+# we get keywords as ID and then check it is keyword or not  
+# so create this list 
 keywords = ["__func__", "__line__", "bool", "break", "btoi",
             "class", "continue", "double", "dtoi", "else", "for", "if",
             "import", "int", "itob", "itod", "new", "NewArray", "null",
@@ -83,6 +84,7 @@ class T(Transformer):
     def STRING_LIT(self, token):
         token = token.replace("$", "*/")
         append_to_prev = False
+        # check end of this string is \" to check have string in string or not 
         if len(token) >= 2 and token[len(token) - 2] == '\\':
             token = token[:-1]
             append_to_prev = True
