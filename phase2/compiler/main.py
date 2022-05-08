6fc06678b5e2421661a564e9835552c6e8bb6fc8
ckodser <arshia.soltani2@gmail.com>
2022-05-08 15:48:51 +0430

    add correct test count

--- phase2/compiler/main.py
+++ phase2/compiler/main.py
@@ -17,26 +17,35 @@ def run(input_file_address: str) -> bool:
 all_tests = {}
 for f in listdir('../tests/in-out'):
     all_tests[f.split('.')[0]] = True
+correct = 0
+error = 0
+total = 0
 
 for step, name in enumerate(all_tests.keys()):
+    total += 1
     try:
         answer = open(f"../tests/in-out/{name}.out").read()
         res = run(f"../tests/in-out/{name}.in")
         if res and answer == "OK":
             print("OK", step)
+            correct += 1
         elif not res and answer != "OK":
             print("OK", step)
+            correct += 1
         else:
             print("WRONG ", step)
     except:
+        error += 1
         print("ERROR on", name)
 
-while True:
-    step = int(input())
-    name = list(all_tests.keys())[step]
-    answer = open(f"../tests/in-out/{name}.out").read()
-    res = run(f"../tests/in-out/{name}.in")
-    print(open(f"../tests/in-out/{name}.in").read())
-    print("name=", name)
-    print("answer=", answer)
-    print("our result=", res)
+print("total=", total, "Correct=", correct, "Wrong=", total-error-correct,"Error=", error)
+
+
+step = int(input("which test to investigate?"))
+name = list(all_tests.keys())[step]
+answer = open(f"../tests/in-out/{name}.out").read()
+text = open(f"../tests/in-out/{name}.in").read()
+print(text)
+print("name=", name)
+print("answer=", answer)
+print(parser(text))
