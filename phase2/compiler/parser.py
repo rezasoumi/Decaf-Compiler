9f60f0f50c2bcd2d92bec58bf110ed25a3ff986f
ckodser <arshia.soltani2@gmail.com>
2022-05-09 11:47:39 +0430

    debug for if stmt

--- phase2/compiler/parser.py
+++ phase2/compiler/parser.py
@@ -3,27 +3,27 @@ from lark import Lark
 json_parser = Lark(r"""
     program: macro* decl+
     macro: "import" ESCAPED_STRING
-    decl: variable_decl | function_decl | class_decl | interface_decl
+    decl: variable_decl | function_decl | class_decl | interface_decl | comment
     variable_decl: variable ";"
     variable: type ident
     type: "int" | "double" | "bool" | "string" | ident | type "[]" 
     function_decl: type ident "(" formals ")" stmtblock | "void" ident "(" formals ")" stmtblock
-    formals: ("," variable)+ | null
+    formals: variable ("," variable)+ |  variable | null
     class_decl: "class" ident ("extends" ident)? ("implements" ("," ident)+)? "{" field* "}"
     field: access_mode variable_decl | access_mode function_decl
     access_mode: "private" | "public" | "protected" | null
     interface_decl: "interface" ident "{" prototype* "}"
     prototype: type ident "(" formals ");" | "void" ident "(" formals ");" 
     stmtblock: "{" linestmtblock* "}"
-    linestmtblock: variable_decl | stmt
+    linestmtblock: variable_decl | stmt | comment
     stmt: expr? ";" | ifstmt | whilestmt | whilestmt | forstmt | breakstmt | continuestmt | returnstmt | printstmt | stmtblock     
-    ifstmt: "if(" expr ")" stmt ("else" stmt)?
-    whilestmt: "while(" expr ")" stmt
-    forstmt: "for(" expr? ";" expr ";" expr? ")" stmt
+    ifstmt: "if""(" expr ")" stmt ("else" stmt)?
+    whilestmt: "while""(" expr ")" stmt
+    forstmt: "for" "(" expr? ";" expr ";" expr? ")" stmt
     returnstmt: "return" expr? ";"
     breakstmt: "break;"
     continuestmt: "continue;"
-    printstmt: "print" ("," expr)+
+    printstmt: "Print" "(" actuals ");"
     expr: lvalue "=" expr | lvalue "+=" expr | lvalue "-=" expr | constant | lvalue | "this" | call | "(" expr ")" | expr "-" expr | expr "+" expr
         | expr "*" expr | expr "/" expr | expr "%" expr | "-" expr | expr "<" expr | expr "<=" expr
         | expr ">" expr | expr ">=" expr | expr "==" expr | expr "!=" expr | expr "&&" expr | expr "||" expr
@@ -33,7 +33,7 @@ json_parser = Lark(r"""
     call: ident "(" actuals ")" | expr "." ident "(" actuals ")" 
     actuals: ("," expr)+  | null
     constant: doubleconstant | INT | boolconstant | ESCAPED_STRING | base16 | "null"
-    
+    comment: "//"/[^\n]+/ | "/*"/[^\*]+/"*/"
     null:
     ident: /[a-zA-Z][a-zA-Z0-9_]*/ | /__func__[a-zA-Z0-9_]*/ | /__line__[a-zA-Z0-9_]*/ 
     doubleconstant: /[0-9]+/"."/[0-9]+/ | /[0-9]+/"." | /[0-9]+/"."/[0-9]*[Ee][+-]?[0-9]+/
@@ -49,4 +49,4 @@ json_parser = Lark(r"""
 def parser(string):
     return json_parser.parse(string)
 
-# parser("int x;")
+print(parser(open(f"../tests/in-out/myfile.in").read()).pretty())
